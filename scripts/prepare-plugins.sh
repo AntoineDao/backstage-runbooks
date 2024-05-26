5231570e304bb18f1a6e550205770dd3d647024f
antoinedao <antoinedao1@gmail.com>
2024-05-26 21:37:09 +0100

    fix(publish): change entrypoints because yarn does not respect "publishConfig"
    Also fixed the dependency setting

---
+++ scripts/prepare-plugins.sh
@@ -0,0 +1,23 @@
+NEW_VERSION=$1
+PACKAGES=("@antoinedao/backstage-plugin-runbook-common" "@antoinedao/backstage-plugin-runbook" "@antoinedao/backstage-plugin-catalog-backend-module-runbook")  # Add your package names here
+
+for file in $(find ./plugins -maxdepth 2 -name package.json); do
+    echo $file
+
+    for package in "${PACKAGES[@]}"; do
+        if jq -e --arg package $package '.dependencies | has($package)' $file > /dev/null; then
+            echo "Found $package in $file and will update to $NEW_VERSION"
+            jq --arg new_version $NEW_VERSION --arg package $package '.dependencies[$package] = $new_version' $file > temp.json && mv temp.json $file
+        fi
+    done
+
+    # The following is required because yarn publish does not respect the "publishConfig" field in package.json
+    # use jq to set "main" to the value in "publishConfig.main" and "types" to the value in "publishConfig.types
+    jq '.main = .publishConfig.main' $file > temp.json && mv temp.json $file
+    jq '.types = .publishConfig.types' $file > temp.json && mv temp.json $file
+
+done
+
+yarn tsc:full
+
+yarn backstage-cli repo build --all
--- scripts/version-plugins.sh
+++
@@ -1,6 +0,0 @@
-NEW_VERSION=$1
-
-for file in $(find ./plugins -maxdepth 2 -name package.json); do
-    echo $file
-    jq --arg version "$NEW_VERSION" '.dependencies."@antoinedao/backstage-plugin-runbook-common" = $version' $file > temp.json && mv temp.json $file
-done
