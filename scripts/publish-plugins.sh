b13208ee47cd7a137fbfb30bf76721ecf193f2fc
antoinedao <antoinedao1@gmail.com>
2024-05-26 15:48:22 +0100

    fix(scripts): stop yarn from tagging when publishing

--- scripts/publish-plugins.sh
+++ scripts/publish-plugins.sh
@@ -1,5 +1,5 @@
 NEW_VERSION=$1
 
-yarn --cwd plugins/catalog-backend-module-runbook publish --new-version $NEW_VERSION
-yarn --cwd plugins/runbook publish --new-version $NEW_VERSION
-yarn --cwd plugins/runbook-common publish --new-version $NEW_VERSION
+yarn --cwd plugins/catalog-backend-module-runbook publish --new-version $NEW_VERSION --no-git-tag-version
+yarn --cwd plugins/runbook publish --new-version $NEW_VERSION --no-git-tag-version
+yarn --cwd plugins/runbook-common publish --new-version $NEW_VERSION --no-git-tag-version
